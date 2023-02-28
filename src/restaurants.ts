0034a6dd9de653f3d7f1b237c4bf27f9dc8896e7
cruelladevil <dev.timetravel@gmail.com>
2023-02-28 12:33:01 +0900

    feat: 클린, 코난 음식점 구현부 작성

--- src/restaurants.ts
+++ src/restaurants.ts
@@ -10,12 +10,13 @@ type Restaurant = {
 const restaurants = [];
 
 const addRestaurant = (restaurant: Restaurant): void => {
-  //
+  restaurants.push(restaurant);
 };
 
-const filterByCategory = (
+const filterByCategoryAndRestaurant = (
   category: Categories,
+  // 클린-코난의 질문: restaurant 객체를 파라미터로 주는 이유가 무엇인가요?
   restaurant: Restaurant
 ): Restaurant[] => {
-  //
+  return restaurants.filter((restaurant) => restaurant.category === category);
 };
